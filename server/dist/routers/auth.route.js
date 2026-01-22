0c766a7f7d58434675a6fc620c9baa2dee21e433
Mohammad Rahmat <r0786j0786r0786a0786@gmail.com>
2026-01-22 17:12:57 +0530

    rebuild

--- server/dist/routers/auth.route.js
+++
@@ -1,6 +0,0 @@
-import e, { Router } from "express";
-import AuthController from "../controllers/auth/auth.controller.js";
-const router = Router();
-router.post('/auth/signup', AuthController.signup);
-export default router;
-//# sourceMappingURL=auth.route.js.map
--- server/src/routers/auth.route.ts
+++
@@ -1,8 +0,0 @@
-import  { Router } from "express";
-import AuthController from "../controllers/auth/auth.controller.js";
-
-const router = Router();
-
-router.post('/auth/signup',AuthController.signup)
-
-export default router;
